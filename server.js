3193cd6f2724cc9da3d4b413cee6c384aae9e29b
mohit9889 <raj.mohit198@gmail.com>
2024-04-24 22:23:37 +0530

    comment removed

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.use(cors({ origin: "http://localhost:3000" }));
 
 // routers
 app.get("/api/v1/hello-world", (req, res) => {
